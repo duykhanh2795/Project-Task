0ff279dc81334a90dfa2703932960f726ed19eb2
duykhanh2795 <157139585+duykhanh2795@users.noreply.github.com>
2024-06-10 18:44:17 +0700

    Update SideBar.tsx

--- src/app/Demo-Project/SideBar.tsx
+++ src/app/Demo-Project/SideBar.tsx
@@ -47,7 +47,7 @@ export default function SideBar({
                     project.id === selectedProjectID
                       ? "break-all"
                       : "text-ellipsis overflow-hidden whitespace-nowrap"
-                  } hover:whitespace-normal hover:break-words`}
+                  } hover:whitespace-normal hover:break-keep`}
                 >
                   {project.title}
                 </button>
